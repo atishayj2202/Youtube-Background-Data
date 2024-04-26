4275db5384d0b6eeb07d06c8b2d6ac052c5696d7
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-04-26 22:27:17 +0530

    Built Youtube Client

---
+++ src/client/youtube.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime, timedelta
+
+from googleapiclient.discovery import build
+
+from src.utils.time import get_current_time, parse_time_datetime_to_tz, parse_time_str_to_tz
+
+
+class YoutubeClient:
+    env_var_api_key = "YOUTUBE_API_KEY"
+    service_name = "youtube"
+    version = "v3"
+
+    def __init__(self):
+        self.api_key = os.environ[self.env_var_api_key]
+        self.youtube = build(self.service_name, self.version, developerKey=self.api_key)
+
+    def search_last_hour(self, query: str):
+        one_minute_ago = (datetime.utcnow() - timedelta(minutes=60))
+        request = self.youtube.search().list(
+            part="snippet",
+            order="date",
+            q=query,
+            type="video",
+            maxResults=50,
+            publishedAfter=one_minute_ago.isoformat() + 'Z'
+        )
+        response = request.execute()
+        return response, parse_time_datetime_to_tz(one_minute_ago)
--- src/utils/time.py
+++ src/utils/time.py
@@ -5,3 +5,9 @@ import pytz
 
 def get_current_time():
     return datetime.now(tz=pytz.utc)
+
+def parse_time_str_to_tz(time_str: str)->datetime:
+    return datetime.fromisoformat(time_str.replace('Z', '+00:00')).astimezone(tz=pytz.utc)
+
+def parse_time_datetime_to_tz(time: datetime)->datetime:
+    return time.astimezone(tz=pytz.utc)
