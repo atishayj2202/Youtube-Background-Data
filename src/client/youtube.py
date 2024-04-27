a156436716275f9caf3dbc02000b09d2b4e31f4f
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-04-28 01:21:14 +0530

    Resolved Issue

--- src/client/youtube.py
+++ src/client/youtube.py
@@ -12,33 +12,37 @@ class YoutubeClient:
     version = "v3"
 
     def __init__(self):
-        self.top = 1
-        self.api_key = os.environ[(self.env_var_api_key + str(self.top))]
-        self.youtube = build(self.service_name, self.version, developerKey=self.api_key)
+        self.top = 0
+        self.youtube = []
+        for i in range(1, 4):
+            self.api_key = os.environ[(self.env_var_api_key + str(i))]
+            self.youtube.append(
+                build(self.service_name, self.version, developerKey=self.api_key)
+            )
 
     def search_last_hour(self, query: str):
         one_minute_ago = datetime.utcnow() - timedelta(minutes=60)
-        request = self.youtube.search().list(
-            part="snippet",
-            order="date",
-            q=query,
-            type="video",
-            maxResults=20,
-            publishedAfter=one_minute_ago.isoformat() + "Z",
+        request = (
+            self.youtube[self.top]
+            .search()
+            .list(
+                part="snippet",
+                order="date",
+                q=query,
+                type="video",
+                maxResults=20,
+                publishedAfter=one_minute_ago.isoformat() + "Z",
+            )
         )
-        if self.top == 4:
+        if self.top == 3:
             return [], one_minute_ago
         try:
             response = request.execute()
             response = response.get("items", [])
-            self.top = 1
+            self.top = 0
             return response, parse_time_datetime_to_tz(one_minute_ago)
         except:
             self.top += 1
-            self.api_key = os.environ[(self.env_var_api_key + str(self.top))]
-            self.youtube = build(
-                self.service_name, self.version, developerKey=self.api_key
-            )
             return self.search_last_hour(query)
 
 
